0d1821a51a3a9a60dcfbb3459b41c429df520dec
juhachiji rei <juhachijirei@gmail.com>
2025-01-28 07:06:47 +0000

    User login base

--- backend/api/v1/routes.go
+++ backend/api/v1/routes.go
@@ -1,26 +1,43 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"FullStackOfYear/backend/handlers/auth"
+	"FullStackOfYear/backend/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(router *gin.RouterGroup) {
-	// user
-	user := router.Group("/users")
+	// auth route
+	authGroup := router.Group("/auth")
 	{
-		user.POST("/register", nil) // TODO
-		user.POST("/login", nil)    // TODO
+		authGroup.POST("/register", auth.Register)
+		authGroup.POST("/login", auth.Login)
 	}
 
-	// comment
-	review := router.Group("/reviews")
+	// route need auth
+	authenticated := router.Group("")
+	authenticated.Use(middleware.Auth())
 	{
-		review.GET("", nil)  // TODO
-		review.POST("", nil) // TODO
-	}
+		// user related
+		user := authenticated.Group("/users")
+		{
+			user.GET("/me", nil) // TODO
+			user.PUT("/me", nil) // TODO
+		}
 
-	// devices
-	device := router.Group("/devices")
-	{
-		device.GET("", nil)  // TODO
-		device.POST("", nil) // TODO
+		// comment related
+		review := authenticated.Group("/reviews")
+		review.Use(middleware.RequireRoles("reviewer", "admin"))
+		{
+			review.GET("", nil)  // TODO
+			review.POST("", nil) // TODO
+		}
+
+		// devices related
+		device := authenticated.Group("/devices")
+		{
+			device.GET("", nil)  // TODO
+			device.POST("", nil) // TODO
+		}
 	}
 }
